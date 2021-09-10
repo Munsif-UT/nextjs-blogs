a2eca487a038384905a9749f4b4f24c615713cee
Muhammad Rizwan Shaikh <razzyshaikh@gmail.com>
2021-09-10 18:10:20 +0500

    Update checkStatus.js

--- config/checkStatus.js
+++ config/checkStatus.js
@@ -1,5 +1,5 @@
 export default function checkStatus() {
   return process.env.NODE_ENV === "development"
     ? "https://devapp.inventooly.com/"
-    : "https://app.inventooly.com/";
+    : "https://devapp.inventooly.com/";
 }
